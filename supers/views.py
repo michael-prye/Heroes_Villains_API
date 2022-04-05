d35a7f3013633781fceb2d84c4e4a851fe90f965
michael-prye <michael.prye@gmail.com>
2022-04-05 11:43:26 -0400

    Created get all endpoints in the views.py for Supers App

--- supers/views.py
+++ supers/views.py
@@ -1,17 +1,32 @@
+from dataclasses import dataclass
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
+from super_types.models import SuperType
 
 @api_view(['GET', 'POST'])
 def all_supers(request):
     if request.method == 'GET':
-        print('it made it')
-        query_set = Super.objects.all()
-        serializer = SuperSerializer(query_set, many =True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        param = request.query_params.get('type')
+        custom_response_dictionary = {}
+        if param:
+            query=Super.objects.filter(super_type__type=param)
+            serializer=SuperSerializer(query,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+
+            hero_query=Super.objects.filter(super_type__type='hero')
+            hero_serializer=SuperSerializer(hero_query, many=True)
+            villain_query=Super.objects.filter(super_type__type='villain')
+            villain_serializer=SuperSerializer(villain_query,many=True)
+            custom_response_dictionary={
+            'hero': hero_serializer.data,
+            'villain': villain_serializer.data
+            }
+            return Response(custom_response_dictionary, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
